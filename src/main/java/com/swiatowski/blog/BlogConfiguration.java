fde80b0109d8ca949f4895022966f5c1f64b2d83
Piotrek <p.swiatowski@gmail.com>
2015-08-15 12:40:46 -0400

    add mongo dependency and mongo config

--- src/main/java/com/swiatowski/blog/BlogConfiguration.java
+++ src/main/java/com/swiatowski/blog/BlogConfiguration.java
@@ -1,9 +1,27 @@
 package com.swiatowski.blog;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yammer.dropwizard.config.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 /**
  * Created by Piotrek on 8/15/2015.
  */
 public class BlogConfiguration extends Configuration {
+
+    @JsonProperty
+    @NotEmpty
+    public String mongohost = "localhost";
+
+    @JsonProperty
+    @Min(1)
+    @Max(65535)
+    public int mongoport = 27017;
+
+    @JsonProperty
+    @NotEmpty
+    public String mongodb = "mydb";
 }
