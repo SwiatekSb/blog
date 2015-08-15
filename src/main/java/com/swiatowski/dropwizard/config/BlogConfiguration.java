6af829bca81ddfbe442a977e3cac3554e1553c0e
Piotrek <p.swiatowski@gmail.com>
2015-08-15 12:08:32 -0400

    Add Configuration class

---
+++ src/main/java/com/swiatowski/dropwizard/config/BlogConfiguration.java
@@ -0,0 +1,9 @@
+package com.swiatowski.dropwizard.config;
+
+import com.yammer.dropwizard.config.Configuration;
+
+/**
+ * Created by Piotrek on 8/15/2015.
+ */
+public class BlogConfiguration extends Configuration {
+}
